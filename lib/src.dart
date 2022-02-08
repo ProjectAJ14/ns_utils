856e700b30222b10b3e46c70f7e36165f2cb47c3
rajan <rajan.metaliya@nonstopio.com>
2022-02-08 12:40:40 +0530

    Remve extension.dart to export all extesion files

--- lib/src.dart
+++ lib/src.dart
@@ -13,8 +13,17 @@ export 'package:recase/recase.dart';
 export 'package:visibility_detector/visibility_detector.dart';
 
 export 'data_type/stackx.dart';
-export 'extensions/extensions.dart';
-
+export 'extensions/context.dart';
+export 'extensions/date_time.dart';
+export 'extensions/double.dart';
+export 'extensions/duration.dart';
+export 'extensions/functions.dart';
+export 'extensions/int.dart';
+export 'extensions/list.dart';
+export 'extensions/map.dart';
+export 'extensions/string.dart';
+export 'extensions/widgets/gesture_detector.dart';
+export 'extensions/widgets/widgets.dart';
 export 'methods/conversion.dart';
 export 'methods/helper.dart';
 export 'services/shared_preferences/sp_service.dart';
